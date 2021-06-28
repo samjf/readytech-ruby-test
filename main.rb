86e3f70b24544dc4dcec647b1115328100188711
Sam F <8619576+samjf@users.noreply.github.com>
2021-06-28 20:44:48 +1200

    load input files

--- main.rb
+++ main.rb
@@ -1,4 +1,12 @@
-def main(checkout_csv_str = '')
+require 'csv'
 
+def main
+  Dir[File.expand_path('input/*.csv')].each { |input_file|
+    csv_file_to_2d_array(input_file) 
+  }
+end
+
+def csv_file_to_2d_array file
+  CSV.read(file, { headers: true })
 end
 
