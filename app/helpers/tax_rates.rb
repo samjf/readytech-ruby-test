e685eae90aded67a41fec861749ac234f0223e16
Sam F <8619576+samjf@users.noreply.github.com>
2021-06-29 11:46:03 +1200

    add exempt products to line items

--- app/helpers/tax_rates.rb
+++ app/helpers/tax_rates.rb
@@ -5,13 +5,22 @@ require 'bigdecimal'
 module TaxRates
   STANDARD_TAX_RATE = 'STANDARD_TAX_RATE'
 
+  EXEMPT_PRODUCT = %w[book chocolate pills].freeze
+
   RATES = {
     STANDARD_TAX_RATE: {
       rate: BigDecimal('0.1')
+    },
+    EXEMPT: {
+      rate: BigDecimal('0')
     }
   }.freeze
 
-  def self.determine_tax_type(_product_name)
-    :STANDARD_TAX_RATE
+  def self.determine_tax_type(product_name)
+    if EXEMPT_PRODUCT.any? { |ex_product| product_name.include?(ex_product) }
+      :EXEMPT
+    else
+      :STANDARD_TAX_RATE
+    end
   end
 end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -4,6 +4,7 @@ require 'app_helper'
 require 'bigdecimal'
 RSpec.describe LineItem do
   let(:checkout_item_misc) { CheckoutItem.new(price: '10.55', product: 'misc product', quantity: 1) }
+  let(:checkout_item_exempt) { CheckoutItem.new(price: '12.55', product: 'chocolate milk', quantity: 1) }
 
   describe '#total' do
   end
@@ -17,6 +18,18 @@ RSpec.describe LineItem do
 
     it 'should have the rate set to standard tax rate decimal format' do
       expect(subject.rate).to eq(BigDecimal('0.1'))
+    end
+
+    context 'given a exempt product' do
+      subject { described_class.new(checkout_item_exempt) }
+
+      it 'should have type set to exempt tax rate' do
+        expect(subject.type).to eq(:EXEMPT)
+      end
+
+      it 'should have the rate set to exempt rate decimal format' do
+        expect(subject.rate).to eq(BigDecimal('0'))
+      end
     end
   end
 end
